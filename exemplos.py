1ec1fb51fc9058284870d69ee2f0688748833df1
Steffanyperes <steffanyperes2017@gmail.com>
2024-06-12 10:48:32 +0900

    steffany brito atividade da calculadora

---
+++ exemplos.py
@@ -0,0 +1,9 @@
+print ("olá mundo")
+a=5
+b=6
+resultado =a+b
+print (resultado)
+print (3*3)
+numero2 = 7.4
+print (numero2)
+#nome das variáveis - regrinhas
---
+++ strings.py
@@ -0,0 +1,18 @@
+nome= "Steffany"
+sobrenome = "Peres"
+print(nome+" "+sobrenome)# só a virgula
+conteudo = "meu ano de  nascimento é "  + str(1998) #tem que colocar str para converter para string
+print(conteudo)
+ano_nascimento = 1998
+idade = 2024- ano_nascimento
+print("minha idade é : " + str(idade))
+valor1=input("digite o valor1: ")
+valor2= input("digite o valor2: ")
+valor1 = int(valor1)
+valor2 = int(valor2)
+soma= valor1 + valor2
+print(soma)
+numero= int(input("digite um numero: "))
+eh_impar=numero%2
+eh_impar= bool(eh_impar)
+print("o numero" , numero , "é impar?" , eh_impar)
